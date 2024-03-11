4cc69384972d7c393e07e44d751f8175304c0c7a
jvdavim <jvdavim@gmail.com>
2024-03-11 19:37:20 -0300

    refactor: webpack directory

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,13 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: {
-    background: path.resolve(__dirname, "..", "src", "background.ts"),
+    background: path.resolve(__dirname, "src", "background.ts"),
   },
   output: {
-    path: path.join(__dirname, "../dist"),
+    path: path.join(__dirname, "./dist"),
     filename: "[name].js",
   },
   resolve: {
@@ -22,8 +23,8 @@ module.exports = {
     ],
   },
   plugins: [
-    new CopyPlugin({
-      patterns: [{ from: ".", to: ".", context: "public" }],
-    }),
+    // new CopyPlugin({
+    //   patterns: [{ from: ".", to: ".", context: "public" }],
+    // }),
   ],
 };
